536f83297d1a9c00750c2f2b4b906d2cd7400479
Alvaro López Medina <alopezme@redhat.com>
2023-10-11 18:51:03 +0200

    :art: Improve documentation and autoinstall for Nexus

--- auto-install-nexus.sh
+++ auto-install-nexus.sh
@@ -14,34 +14,33 @@ NEXUS_SERVER_NAME="nexus-server"
 echo -e "\n=============="
 echo -e "ENVIRONMENT VARIABLES:"
 echo -e " * OPERATOR_NAMESPACE: $OPERATOR_NAMESPACE"
+echo -e " * NEXUS_SERVER_NAME: $NEXUS_SERVER_NAME"
 echo -e "==============\n"
 
 # Check if the user is logged in 
 if ! oc whoami &> /dev/null; then
-    echo -e "Check. You are not logged in. Please log in and run the script again."
+    echo -e "Check. You are not logged. Please log in and run the script again."
     exit 1
 else
     echo -e "Check. You are correctly logged in. Continue..."
-    oc project default
+    if ! oc project &> /dev/null; then
+        echo -e "Current project does not exist, moving to project Default."
+        oc project default 
+    fi
 fi
 
 # Install Nexus operator
-echo -e "\n[1/4]Install Nexus operator"
+echo -e "\n[1/3]Install Nexus operator"
 
-oc process -f templates/nexus-01-operator.yaml \
+oc process -f templates/nexus/01-operator.yaml \
     -p OPERATOR_NAMESPACE=$OPERATOR_NAMESPACE | oc apply -f -
 
-echo -n "\tWaiting for pods ready..."
+echo -n "Waiting for pods ready..."
 while [[ $(oc get pods -l name=nxrm-operator-certified -n $OPERATOR_NAMESPACE -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
 
-# Provide priviledged SCC to the pod
-# Workaround for issue: https://github.com/sonatype/operator-nxrm3/issues/8
-echo -e "\n[2/4]Provide priviledged SCC to the pod"
-oc adm policy add-scc-to-user privileged -z default -n $OPERATOR_NAMESPACE
-
 # Deploy the Nexus repository manager instance
-echo -e "\n[3/4]Deploy the Nexus repository manager instance"
-oc process -f templates/nexus-02-server.yaml \
+echo -e "\n[2/3]Deploy the Nexus repository manager instance"
+oc process -f templates/nexus/02-server.yaml \
     -p OPERATOR_NAMESPACE=$OPERATOR_NAMESPACE \
     -p SERVER_NAME="$NEXUS_SERVER_NAME" | oc apply -f -
 
@@ -53,7 +52,7 @@ NEXUS_ROUTE=$(oc get routes $NEXUS_SERVER_NAME -n $OPERATOR_NAMESPACE --template
 
 # Create Helm repository
 # For full doc of the API, check the Swagger doc
-echo -e "\n[4/4]Create Helm repository to host packaged Helm charts"
+echo -e "\n[3/3]Create Helm repository to host packaged Helm charts"
 
 curl -u admin:admin123 -X 'POST' \
   "$NEXUS_ROUTE/service/rest/v1/repositories/helm/hosted" \
